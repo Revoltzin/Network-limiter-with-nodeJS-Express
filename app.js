18be89008fd533629cc792d210bf03a5ff9bbfc1
Matheus Tresguerras <97244120+Revoltzin@users.noreply.github.com>
2025-09-20 21:03:56 +0000

    Application

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
 const port = 5000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 5, // Limit each ip to 5 requests
+  message:
+    "Too many API requests from this IP, please try again after 15 minutes",
+});
+
+const loginAccountLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 5, // Limit each ip to 5 requests
+  message: "Try again after 15 minutes",
+});
+
+// This definition allows to be used in all routes
+// app.use(limiter);
+
+app.use("/api", limiter);
 
 app.get("/api/v1", function (req, res) {
   res.json([
@@ -12,6 +31,10 @@ app.get("/api/v1", function (req, res) {
       Description: "Javascript ruin v8",
     },
   ]);
+});
+
+app.get("/login", loginAccountLimiter, function (req, res) {
+  res.send("Imaginary login form.");
 });
 
 app.listen(port, () => {
